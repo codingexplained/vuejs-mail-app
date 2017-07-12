a54b19015f317ad6bf5b544a797eced2f280bbfe
Bo Andersen <ba0708@icloud.com>
2017-07-12 14:04:17 +0200

    Added button to refresh the inbox with random message

--- src/App.vue
+++ src/App.vue
@@ -11,12 +11,21 @@
     import Sidebar from './Sidebar.vue';
     import Content from './Content.vue';
     import messages from './data/messages';
+    import randomMessages from './data/random-messages';
+    import { eventBus } from './main';
 
     export default {
         data() {
             return {
                 messages: messages
             };
+        },
+        created() {
+            eventBus.$on('refreshMessages', () => {
+                let randomIndex = Math.floor(Math.random() * randomMessages.length);
+                let temp = [randomMessages[randomIndex]];
+                this.messages = temp.concat(this.messages.slice(0));
+            });
         },
         components: {
             appSidebar: Sidebar,
--- src/Inbox.vue
+++ src/Inbox.vue
@@ -1,10 +1,19 @@
 <template>
     <div class="inbox-body">
+        <div class="mail-option">
+            <div class="btn-group">
+                <a href="#" class="btn" @click="refresh">
+                    <i class="fa fa-refresh"></i>&nbsp; Refresh
+                </a>
+            </div>
+        </div>
+
         <app-messages :messages="incomingMessages"></app-messages>
     </div>
 </template>
 
 <script>
+    import { eventBus } from './main';
     import Messages from './Messages.vue';
 
     export default {
@@ -12,6 +21,11 @@
             data: {
                 type: Object,
                 required: true
+            }
+        },
+        methods: {
+            refresh() {
+                eventBus.$emit('refreshMessages');
             }
         },
         computed: {
