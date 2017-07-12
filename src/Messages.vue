0552f5d06aaf25d7cedf2b4a212af60d47cfadda
Bo Andersen <ba0708@icloud.com>
2017-07-12 13:44:54 +0200

    Implemented viewing messages

--- src/Messages.vue
+++ src/Messages.vue
@@ -1,7 +1,7 @@
 <template>
     <table v-if="messages.length > 0" class="table table-inbox table-hover">
         <tbody>
-            <tr v-for="message in messages" :class="{ unread: typeof message.isRead !== 'undefined' && !message.isRead }">
+            <tr v-for="message in messages" @click="openMessage(message)" :class="{ unread: typeof message.isRead !== 'undefined' && !message.isRead }">
                 <td><input type="checkbox"></td>
                 <td>
                     <a href="#" v-if="typeof message.isImportant !== 'undefined'" @click.prevent.stop="message.isImportant = !message.isImportant">
@@ -20,11 +20,24 @@
 </template>
 
 <script>
+    import { eventBus } from './main';
+
     export default {
         props: {
             messages: {
                 type: Array,
                 required: true
+            }
+        },
+        methods: {
+            openMessage(message) {
+                eventBus.$emit('changeView', {
+                    tag: 'app-view-message',
+                    title: message.subject,
+                    data: {
+                        message: message
+                    }
+                });
             }
         }
     }
--- src/ViewMessage.vue
+++ src/ViewMessage.vue
@@ -1,9 +1,45 @@
 <template>
-    <h1>ViewMessage</h1>
+    <div class="inbox-body">
+        <p><strong>Date:</strong> {{ data.message.date.fromNow() }}</p>
+        <p><strong>From:</strong> {{ data.message.from.name }} <{{ data.message.from.email }}></p>
+        <hr>
+
+        <div v-html="data.message.content" class="message"></div>
+
+        <div v-if="data.message.attachments.length > 0" class="attachments">
+            <h4>Attachments</h4>
+
+            <ul>
+                <li v-for="attachment in data.message.attachments">
+                    <i class="fa fa-paperclip"></i> {{ attachment.fileName }} ({{ attachment.size | formatBytes }})
+                </li>
+            </ul>
+        </div>
+    </div>
 </template>
 
 <script>
     export default {
+        props: {
+            data: {
+                type: Object,
+                required: true
+            },
+        },
+        filters: {
+            formatBytes(bytes) {
+                if (bytes == 0) {
+                    return '0 Bytes';
+                }
 
+                let decimals = 2;
+                let k = 1000;
+                let dm = decimals + 1 || 3;
+                let sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'];
+                let i = Math.floor(Math.log(bytes) / Math.log(k));
+
+                return parseFloat((bytes / Math.pow(k, i)).toFixed(dm)) + ' ' + sizes[i];
+            }
+        }
     }
 </script>
